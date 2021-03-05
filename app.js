e491d426d90e53c7ca701a3219e504e086f49ea5
Irisjansen03 <66818995+Irisjansen03@users.noreply.github.com>
2021-03-05 12:12:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 const questionNumber = document.querySelector(".question-number");
 const questionText = document.querySelector(".question-text");
-const optionContainer = document.querySelector("option-container");
+const optionContainer = document.querySelector(".option-container");
 
 Let questionCounter = 0;
 Let currentQuestion;
 Let availableQuestions = [];
+Let availableOptions = [];
 
 // push the questions into availableQuestions Array
 function setAvailableQuestions(){
@@ -26,8 +27,38 @@ function getNewQuestion(){
   const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
   currentQuestion = questionIndex;
   questionText.innerHTML = currentQuestion.q;
-// console.log(quenstionIndex)
+// get the position of 'questioIndex' from the availableQuestion Array
+  const index1= availableQuestions.indexOf(questionIndex);
+  // remove the 'questionIndex' from the availableQuestion Array, so that the question does not repeat
+   availableQuestions.splice(index1,1);
+
+ // set options
+  // get the length of options
+  const optionLen = currentQuestion.options.length
+  // push options into availableOptions Array
+for(Let i=0; i<optionLen; i++){
+  availableOptions.push(i)
+}
+// create options in html
+  for(Let i=0; i<optionLen; i++){
+    const option =  document.createElement("div");
+    option.innerHTML = currentQuestion.options[i];
+    option.id = i;
+    option.className = "option"
+    optionContainer.appendChild(option)
   }
+  
+  questionCounter++
+  }
+
+function next(){
+ if(questionCounter === quiz.lenght){
+  console.log("quiz over")
+ }
+  else{ 
+   getNewQuestion(); 
+  }
+}
 
 window.onload = function(){
   // first we will set all questions in availableQuestions Array

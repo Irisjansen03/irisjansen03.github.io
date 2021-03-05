77e060fc7e050b518cc3a4b608bf4468e70b770c
Irisjansen03 <66818995+Irisjansen03@users.noreply.github.com>
2021-03-05 11:39:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 
 
-console.log(quiz[0])
+const questionNumber = document.querySelector(".question-number");
+const questionText = document.querySelector(".question-text");
+const optionContainer = document.querySelector("option-container");
+
+Let questionCounter = 0;
+Let currentQuestion;
+Let availableQuestions = [];
+
+// push the questions into availableQuestions Array
+function setAvailableQuestions(){
+  const totalQuestions = quiz.length;
+  for(Let i-0; i<totalQuestion; i++){
+    availableQuestions.push(quiz[i])
+  }
+}
+
+// set question number and question and options
+function getNewQuestion(){
+  // set question number
+  questionNumber.innerHTML = "Question" + (questionCounter + 1) + "of" + quiz.length;
+  
+  // set question text
+  // get random question
+  const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
+  currentQuestion = questionIndex;
+  questionText.innerHTML = 
+// console.log(quenstionIndex)
+  }
+
+window.onload = function(){
+  // first we will set all questions in availableQuestions Array
+ setAvailableQuestions();
+  // second we will call getNewQuestion(); function
+  getNewQuestion();
+}

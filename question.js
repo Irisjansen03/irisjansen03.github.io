e1f23bd8ab34f3839f742c5d707fec2bc63db970
Irisjansen03 <66818995+Irisjansen03@users.noreply.github.com>
2021-03-05 11:16:13 +0100

    Update question.js

--- question.js
+++ question.js
@@ -3,9 +3,29 @@
 // Array of Objects
 const quiz = [
   {
-    q:'',
-    options:[]
-    answer:
-  }
-  
+    q:'Uit welk land komt een pizza?',
+    options:['Italië','Frankrijk','Amerika','Nederland'],
+    answer:0
+  },
+    {
+    q:'Welke band is hoger bij judo?',
+    options:['Groene band','Gele band','Witte band','Paarse band'],
+    answer:1
+  },
+  {
+    q:'Hoeveel nullen heeft een miljoen?',
+    options:['6','9'],
+    answer:0
+  },
+    {
+    q:'Waar zit je trommelvlies?',
+    options:['Oor','Oog','Neus'],
+    answer:2
+  },
+  {
+    q:'WWat is duurder bij de Mc. Donalds?',
+    options:['Frietjes','Hamburger','Cheeseburger','Frietsaus'],
+    answer:3
+  },
+
   ]

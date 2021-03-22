fa07565c3020567246dad18ac5f7aaf2f646f186
Irisjansen03 <66818995+Irisjansen03@users.noreply.github.com>
2021-03-22 11:31:40 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,69 +1,191 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Quiz App</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
 
-<div class="home-box custom-box">
-<h3>Instruction:</h3>
-<p>Total number of questions: <span class="total-questions"></span><p>
-<button type="button" class="btn" onclick="startQuiz()">Start Quiz</button>
-</div>
 
-<div class="quiz-box custom-box hide">
-  <div class="question-number">
+const questionNumber = document.querySelector(".question-number");
+const questionText = document.querySelector(".question-text");
+const optionContainer = document.querySelector(".option-container");
+const anwersIndicatorContainer = document.querySelector(".answers-indicator");
+const homeBox = document.querySelector(".home-box");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
 
-    </div>
-  <div class="question-text">
+Let questionCounter = 0;
+Let currentQuestion;
+Let availableQuestions = [];
+Let availableOptions = [];
+Let correctAnswers = 0;
+Let attempt = 0;
 
-  </div>
-  <div class="option-container">
+// push the questions into availableQuestions Array
+function setAvailableQuestions(){
+  const totalQuestions = quiz.length;
+  for(Let i-0; i<totalQuestion; i++){
+    availableQuestions.push(quiz[i])
+  }
+}
 
-  </div>
-  <div class="next-question-btn">
-    <button type="button" class="btn" onclick="next()">Next</button>
-  </div>
-  <div class="answers-indicator">
-
-  </div>
-</div>
-<div class="result-box custom-box hide">
-  <h1>Quiz Result</h1>
-  <table>
-    <tr>
-      <td>Total Question</td>
-      <td><span class="total-question"></span></td>
-</tr>
-    <tr>
-      <td>Attempt</td>
-      <td><span class="total-attempt"></span></td>
-</tr>
-    <tr>
-      <td>Correct</td>
-      <td><span class="total-correct"></span></td>
-</tr>
-    <tr>
-      <td>Wrong</td>
-      <td><span class="total-wrong"></span></td>
-</tr>
-    <tr>
-      <td>Percentage</td>
-      <td><span class="percentage"></span></td>
-</tr>
-    <tr>
-      <td>Your Total Score</td>
-      <td><span class="total-score"></span></td>
-</tr>
-  </table>
-    <button type="button" class="btn" onclick="tryAgainQuiz()">Try Again</button>
-    <button type="button" class="btn" onclick="goToHome()">Go To Home</button>
-</div>
-
+// set question number and question and options
+function getNewQuestion(){
+  // set question number
+  questionNumber.innerHTML = "Question" + (questionCounter + 1) + "of" + quiz.length;
   
-  <script src="js/questions.js"></script>
-  <script src="js/app.js"></script>
-</body>
-</html>
+  // set question text
+  // get random question
+  const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
+  currentQuestion = questionIndex;
+  questionText.innerHTML = currentQuestion.q;
+// get the position of 'questioIndex' from the availableQuestion Array
+  const index1= availableQuestions.indexOf(questionIndex);
+  // remove the 'questionIndex' from the availableQuestion Array, so that the question does not repeat
+   availableQuestions.splice(index1,1);
+
+ // set options
+  // get the length of options
+  const optionLen = currentQuestion.options.length
+  // push options into availableOptions Array
+for(Let i=0; i<optionLen; i++){
+  availableOptions.push(i)
+}
+  optionContainer.innerHTML = '';
+  Let animationDelay = 0,15;
+// create options in html
+  for(Let i=0; i<optionLen; i++){
+    // random option
+    const optonIndex = availableOptions[Math.floor(Math.random() * availableOptions.length]
+    // get the position of 'optonIndex' from the availableOptions Array
+    const index2= availableOptions.indexOf(optonIndex);
+    // remove the'optonIndex' from the availableOptions Array , so that the option does not repeat
+    availableOptions.splice(index2,1);                       
+    const option =  document.createElement("div");
+    option.innerHTML = currentQuestion.options[optonIndex];
+    option.id = optonIndex;
+    option.style.animationDelay = 'animationDelay' + 's';
+    animationDelay = animationDelay + 0.15;
+    option.className = "option"
+    optionContainer.appendChild(option)
+    option.setAttribute("onclick","getResult(this)");
+  }
+  
+  questionCounter++
+  }
+
+// get the result of current attempt question
+function getResult(element){
+ const id = parseInt(element.id);
+// get the answer by comparing the id of clicked option
+  if(id === currentQuestion.answer){
+    // set the green color to the correct option
+   element.classList.add("correct");
+    // add the indicator to correct mark
+    updateAnswerIndicator("correct");
+    correctAnswers++;
+  }
+  else{
+        // set the red color to the incorrect option
+    element.classList.add("wrong");
+    // add the indicator to wrong mark
+    updateAnswerIndicator("wrong");
+  
+  // if the answer is incorrect the show the correct option by adding green color the correct option
+  const optioLen = optionContainer.children.length;
+    for(Let i=0; i<optionLen; i++){
+      if(ParseInt(optionContainer.children[i].id) === currentQuestion.answer )
+        optionContainer.children[i].element.classList.add("correct")
+    }
+    
+  }
+}
+attempt++;
+  unclickableOptions();
+}
+
+
+//  make all the options unclickable once the user select a option (RESTRICT THE USER TO CHANGE THE OPTION AGAIN)
+function unclickableOptions();{
+  const optionLen = optionContainer.children.length;
+  for(Let i=0 ; i<optionLen; i++){
+    optionContainer.children[i].classList.add("already-answered")
+  }
+}
+function   answerIndicatior(){
+  answerIndicatorContainer.innerHTML = '';
+  const totalQuestion = quiz.length;
+  for(Let i=0; i<totalQuestion; i++){
+   const indicator = document.createElement ("div");
+    answerIndicatorContainer.appendChild(indicator);
+  }
+}
+function updateAnswerIndicator(markType);{
+  answerIndicatorContainer.children[questionCounter-1].classlist.add(markType)
+}
+
+function next(){
+ if(questionCounter === quiz.lenght){
+  quizOver();
+ }
+  else{ 
+   getNewQuestion(); 
+  }
+}
+
+function quizOver(){
+  // hide quiz Box
+  quizBox.classList.add("hide");
+ // show result Box 
+  resultBox.classlist.remove("hide");
+  quizResult();
+}
+// get the quiz Result
+function quizResult(){
+  resultBox.querySelector(".total-question").innerHTML = quiz.length;
+  resultBox.querySelector(".total-attempt").innerHTML = attempt;
+  resultBox.querySelector(".total-correct").innerHTML = correctAnswers;
+  resultBox.querySelector(".total-wrong").innerHTML = attempt - correctAnswers;
+  const percentage = (correctAnswers/quiz.length)*100;
+  resultBox.querySelector(".percentage").innerHTML = percentage.toFixed(2) + "%";
+  resultBox.querySelector(".total-score").innerHTML = correctAnswers +" / " + quiz.length;
+}
+
+function resetQuiz(){
+  questionCounter = 0;
+  correctAnswers = 0;
+  attempt = 0;
+}
+
+function tryAgainQuiz (){
+ // hide the resultBox 
+  resultBox.classList.add("hide");
+  // show the quizBox
+  quizBox.classList.remove("hide");
+  resetQuiz();
+  startQuiz();
+}
+
+function goToHome(){
+ // hide result Box 
+resultBox.classList.add("hide");
+ // show home box
+  homeBox.classList.remove("hide");
+   resetQuiz();
+}
+
+// #### STARTING POING ####
+
+function startQuiz(){
+  
+  // hide home box
+  homeBox.classList.add("hide");
+  // show quiz Box
+  quizBox.classList.remove("hide");
+  // first we will set all questions in availableQuestions Array
+ setAvailableQuestions();
+  // second we will call getNewQuestion(); function
+  getNewQuestion();
+  // to create indicator of answers
+  answerIndicatior();
+
+}
+
+
+window.onload = function (){
+ homeBox.querySelector(".total-question").innerHTML = quiz.length; 
+}
